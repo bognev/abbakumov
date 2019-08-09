ecf4c68d7c921ed2c36d768afbbe35126484904c
zabolotsky <bognev@mail.ru>
2019-08-09 16:13:41 +0300

    lec2

--- lec1.py
+++ lec1.py
@@ -14,11 +14,18 @@ print(AH.describe(include='all'))
 # AH['SalePrice'].hist()
 print(style.available)
 # fig, ax = plt.subplots()
-plt.hist(AH['SalePrice'], bins=60, log=True)#.log.hist(bins=60, density=1)
+# plt.hist(AH['SalePrice'], bins=60, log=True)#.log.hist(bins=60, density=1)
 
 from scipy.stats.kde import gaussian_kde
 from numpy import linspace, hstack
 from pylab import plot, show, hist
 
+my_density = gaussian_kde(AH['SalePrice'])
+x = linspace(min(AH['SalePrice']), max(AH['SalePrice']), 1000)
+# plot(x, my_density(x), 'g')
+# hist(AH['SalePrice'], normed=1, alpha=0.3)
+# AH.groupby('MS Zoning')['SalePrice'].plot.hist(density=1, alpha=0.6)
+ax = AH.boxplot(column='SalePrice', by='MS Zoning')
+ax.get_figure().suptitle('')
 plt.show()
 # print(AH.head())

41bc683f4ec33a4742f548ca1560910f7d2ebf91
zabolotsky <bognev@mail.ru>
2019-08-27 18:22:42 +0300

    exactly what i was looking for

---
+++ lec3.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.style.use("ggplot")
+import os
+
+df = pd.read_csv("./Shad_AD on Python_1_01/1_beverage/beverage_r.csv", sep=";", index_col='numb.obs')
+
+print(df.tail())
+print(df.shape)
+
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+link = linkage(df, 'ward', 'euclidean') #кластерный анализ #расстояние между кластерами и #объектами
+print(link)
+dn = dendrogram(link, orientation="right")
+
+df['cluster'] = fcluster(link, 3, criterion='distance')
+print(df.groupby('cluster').mean())#какая доля людей пила данный напиток в данном кластере
+print(df.groupby('cluster').size())
+plt.show()
+
+

e3f0847c0ef65cf592c301c0ed36b1b45badd161
zabolotsky <bognev@mail.ru>
2019-09-25 21:03:33 +0300

    Lec5

--- lec4_2.py
+++ lec4_2.py
@@ -6,5 +6,9 @@ from sklearn.cluster import KMeans
 from matplotlib import style
 style.use("ggplot")
 
-df = pd.read_csv("./home/zabolotsky/PycharmProjects/abbakumov/Shad_AD on Python_1_01/2_pretendent/assess.dat", sep="\t", index_col='NAME')
+df = pd.read_csv("/home/zabolotsky/PycharmProjects/abbakumov/Shad_AD on Python_1_01/2_pretendent/assess.dat", sep="\t", index_col='NAME')
 print(df.head())
+model = KMeans(n_clusters=4, random_state=42)
+model.fit(df)
+df['cluster'] = model.labels_
+print(df.groupby('cluster').mean())
---
+++ lec5.py
@@ -0,0 +1,5 @@
+#                     hypothesis true hypothesis false
+#hypothesis accepted        +        2nd type error
+#hypothesis rejected  1st type error        +
+#alpha significance = 0.05, 0.01, 0.005 margin from top for frequency of 1st type mistakes
+#it will give us 1 mistake from 20, 100, 200 checks
